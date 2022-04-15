49463fe684ff842fd5bbddce08f1f2b7603f450c
yatu360 <yatu360@gmail.com>
2022-04-15 20:58:34 +0100

    004 Instruction SubClasses

---
+++ src/sml/instructions/OutInstruction.java
@@ -0,0 +1,25 @@
+package sml.instructions;
+
+import sml.Instruction;
+import sml.Machine;
+
+public class OutInstruction extends Instruction {
+    /**
+     * Constructor: an instruction with label l and opcode op
+     * (op must be an operation of the language)
+     *
+     * @param l        label
+     * @param op       operand
+     * @param register
+     * @param s1
+     * @param s2
+     */
+    public OutInstruction(String l, String op, int register, int s1, int s2) {
+        super(l, op, register, s1, s2);
+    }
+
+    @Override
+    public void execute(Machine m) {
+
+    }
+}
