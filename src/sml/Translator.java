8c284c8ed39bcea1f91f42a45f90d175554faef0
yatu360 <yatu360@gmail.com>
2022-04-16 11:16:31 +0100

    006 Switch Statement

--- src/sml/Translator.java
+++ src/sml/Translator.java
@@ -1,6 +1,6 @@
 package sml;
 
-import sml.instructions.AddInstruction;
+import sml.instructions.*;
 
 import java.io.File;
 import java.io.IOException;
@@ -92,6 +92,36 @@ public final class Translator {
                 s1 = scanInt();
                 s2 = scanInt();
                 return new AddInstruction(label, r, s1, s2);
+            }
+            case "sub" -> {
+                r = scanInt();
+                s1 = scanInt();
+                s2 = scanInt();
+                return new SubInstruction(label, r, s1, s2);
+            }
+            case "bnz" -> {
+                r = scanInt();
+                s1 = scanInt();
+                s2 = scanInt();
+                return new BnzInstruction(label, r, s1, s2);
+            }
+            case "div" -> {
+                r = scanInt();
+                s1 = scanInt();
+                s2 = scanInt();
+                return new DivInstruction(label, r, s1, s2);
+            }
+            case "mul" -> {
+                r = scanInt();
+                s1 = scanInt();
+                s2 = scanInt();
+                return new MulInstruction(label, r, s1, s2);
+            }
+            case "out" -> {
+                r = scanInt();
+                s1 = scanInt();
+                s2 = scanInt();
+                return new OutInstruction(label, r, s1, s2);
             }
             // TODO: You will have to write code here for the other instructions.
 
